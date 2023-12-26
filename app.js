53b9842380f664c7b8cac5f0d507902e08b0465c
Jayesh421 <106749064+Jayesh421@users.noreply.github.com>
2023-12-26 17:26:39 +0530

    for loop for list

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const bodyParser = require("body-parser");
 var app = express();
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
-
-var example = "Working";
+var items = [];
+var example = "working";
 app.get("/",function(req,res){
-    res.render("list",{exej : example});
+    res.render("list",{ejes : items});
 });
 
 app.post("/",function(req,res){
-    console.log(req.body.ele1);
+    var item = req.body.ele1;
+    items.push(item);
+    res.redirect("/"); 
 });
 
 app.listen(8000,function(){
