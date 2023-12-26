72398a8f6fd76ffb67352ad25c39bb5af675632d
Jayesh421 <106749064+Jayesh421@users.noreply.github.com>
2023-12-26 17:01:34 +0530

    sucess

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 var app = express();
-app.set("view enagine","ejs");
-app.use(express.urlencoded({extended:true}))
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}));
 
 var example = "Working";
 app.get("/",function(req,res){
-    res.render("list",{exej : example})
+    res.render("list",{exej : example});
 });
 
 app.post("/",function(req,res){
-    console.log(req.body.ele1)
+    console.log(req.body.ele1);
 });
 
 app.listen(8000,function(){
-    console.log("Server Stared")
+    console.log("Server Stared");
 });
