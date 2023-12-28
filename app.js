a0c5bf6e98352baac16e91d39b6409c8668304e4
Jayesh421 <106749064+Jayesh421@users.noreply.github.com>
2023-12-28 17:08:41 +0530

    database connection completed

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const express = require("express");
 const bodyParser = require("body-parser");
-
-var app = express();
-app.set("view engine","ejs");
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
 /*var items = [];
 var example = "working";
 app.get("/",function(req,res){
@@ -20,25 +14,77 @@ app.post("/",function(req,res){
 app.listen(8000,function(){
     console.log("Server Stared");
 });*/
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect("mongodb://localhost:27017/todo");
+
 const trySchema = new mongoose.Schema({
-    name:String
+    name: String
 });
-const item = mongoose.model("task",trySchema);
-const todo = new item({
-    name:"Create Some Videos"
-}); 
-const todo2 = new item({
-    name:"Learn DSA"
-});
-const todo3 = new item({
-    name:"Learn React"
-});
-const todo4 = new item({
-    name:"Take Some Rest"
-});
-todo.save;
-todo2.save;
-todo3.save;
-todo4.save;
+
+const Item = mongoose.model("Item", trySchema); // Use uppercase for model names by convention
+
+/*const todo = new Item({ name: "Create Some Videos" });
+const todo2 = new Item({ name: "Learn DSA" });
+const todo3 = new Item({ name: "Learn React" });
+const todo4 = new Item({ name: "Take Some Rest" });
+*/
+// Save the items to the database
+/* todo.save()
+    .then(() => {
+        console.log("Todo saved");
+    })
+    .catch((err) => {
+        console.error("Error saving Todo:", err);
+    });
+
+todo2.save()
+    .then(() => {
+        console.log("Todo2 saved");
+    })
+    .catch((err) => {
+        console.error("Error saving Todo2:", err);
+    });
+
+todo3.save()
+    .then(() => {
+        console.log("Todo3 saved");
+    })
+    .catch((err) => {
+        console.error("Error saving Todo3:", err);
+    });
+
+todo4.save()
+    .then(() => {
+        console.log("Todo4 saved");
+    })
+    .catch((err) => {
+        console.error("Error saving Todo4:", err);
+    });*/
+    app.get("/", async function(req, res) {
+        try {
+            const foundItems = await Item.find({});
+            res.render("list", { dayej: foundItems });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+    app.post("/",function(req,res){
+        const itemName = req.body.ele1;
+        const todo = new Item({
+            name:itemName
+        });
+        todo.save();
+        res.redirect("/");
+    });
+    
+    app.listen(3000, function() {
+        console.log("Server is running on port 3000");
+    });
